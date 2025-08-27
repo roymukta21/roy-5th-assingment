4961af746efa6d6cd8f3a932ad4e9e6d576c3a37
hira roy <muktaroy520@gmail.com>
2025-08-27 22:52:19 +0600

    I creat a js folder.

---
+++ script.js
@@ -0,0 +1,50 @@
+//heart counter
+
+const heartCountEl = document.getElementById("heartCount");
+let heartCount = 0;
+const cardHearts = document.querySelectorAll(".card-heart");
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("card-heart")) {
+    heartCountEl.textContent = ++heartCount;
+    e.target.classList.toggle("text-red-500");
+  }
+});
+
+// call+clear section
+
+const historyList = document.getElementById("historyList");
+const clearBtn = document.querySelector(".clear-btn");
+
+// Handle Call button clicks
+document.querySelectorAll(".call-btn").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const card = btn.closest(".bg-white");
+    const serviceName = card.querySelector("h2").textContent;
+    const number = card.querySelector("p.font-bold").textContent;
+
+    // call Time
+    const time = new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+
+    // new entry
+    const entry = document.createElement("div");
+    entry.className =
+      "border-b border-gray-200 pb-1 flex justify-between items-center";
+    entry.innerHTML = `
+      <div>
+        <span class="font-bold">${serviceName}</span> 
+        <span class="text-gray-500">(${number})</span>
+      </div>
+      <span class="text-xs text-gray-400">${time}</span>
+    `;
+    historyList.prepend(entry);
+  });
+});
+
+// Handle Clear button
+clearBtn.addEventListener("click", () => {
+  historyList.innerHTML = "";
+});
